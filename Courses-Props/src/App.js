8aa57b97dd0bb3ed3394c6cf2269164dd4739332
server_42 <devbhattacharya42@gmail.com>
2023-07-24 23:36:15 +0530

    Assigmment 2 scored 7

--- Courses-Props/src/App.js
+++ Courses-Props/src/App.js
@@ -1,5 +1,18 @@
 // import Courses ,Title and UserCard here
 // keep a user object with mentioned properties and pass down to UserCard as prop
+import Title from './Components/Title'
+import UserCard from './Components/UserCard'
+import Courses from './Components/Courses'
 export default function App() {
-  return <></>;
+  return(
+    <div>
+      <div>
+      <Title />
+      </div>
+      <div>
+        <UserCard name='Dev' avatar='https://dummyimage.com/200x200.jpg/ff4444/ffffff' posts='2' followers='100' address='Shyamnagar'/>
+        <Courses />
+      </div>
+    </div>
+  )
 }
--- Courses-Props/src/Components/Title.jsx
+++ Courses-Props/src/Components/Title.jsx
@@ -1,3 +1,16 @@
 // Your code goes here
 // do a deafult export
+import React from 'react'
+// import './title.css'
+export default function Title() {
+  let style = {
+      color:'teal'
+  }
+  return (
+    <div className='Title'>
+        <h1 style={style}>Users Display</h1>
+    </div>
+  )
+}
+
 
