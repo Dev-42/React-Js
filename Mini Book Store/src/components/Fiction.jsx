eabd1464258eb7a10b7218031301dfea120e5658
Dev Bhattacharya <71654514+Dev-42@users.noreply.github.com>
2023-08-01 07:16:56 +0530

    Update Fiction.jsx

--- Mini Book Store/src/components/Fiction.jsx
+++ Mini Book Store/src/components/Fiction.jsx
@@ -1,10 +1,29 @@
+import {useState} from 'react'
+import BookCard from './BookCard';
 export default function Fiction() {
+  const [books,setBooks] = useState([])
+
+  let booksDisplay = async () => {
+    try{
+      let res = await fetch('http://localhost:8000/fiction')
+      let data = await res.json();
+      setBooks(data)
+      console.log(data)
+  }
+  catch(e){
+    console.log(e);
+  }
+}
   return (
     <div data-testid='books-fiction'>
-      <h1 data-testid='books-container-title'>{}</h1>
+      <h1 data-testid='books-container-title' style={{textAlign:'center'}}>Fictional Books</h1>
+      <button onClick={booksDisplay}>Click Here!!!</button>
 
-      <div className="books-container">
+      <div className="books-container" style={{display:'flex',justifyContent:'space-evenly'}}>
         {/* Map all Fictional Books here */}
+        {books.map((book,index) => (
+          <BookCard key = {index} props = {book} />
+        ))}
       </div>
     </div>
   );
