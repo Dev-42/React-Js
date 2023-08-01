41b6bbfddc2477c62b569f6332d0f267d706218d
Dev Bhattacharya <71654514+Dev-42@users.noreply.github.com>
2023-08-01 07:17:19 +0530

    Update NonFiction.jsx

--- Mini Book Store/src/components/NonFiction.jsx
+++ Mini Book Store/src/components/NonFiction.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react"
+import BookCard from './BookCard'
 export default function NonFiction() {
+  const [books,setBooks] = useState([])
+
+  let booksDisplay = async () => {
+    try{
+      let res = await fetch('http://localhost:8080/nonfriction')
+      let data = await res.json()
+      setBooks(data)
+      console.log(data)
+    }catch(e){
+      console.log(e)
+    }
+  }
+
   return (
     <div data-testid='books-nonfiction'>
-      <h1 data-testid='books-container-title'>{}</h1>
-
-      <div className="books-container">
+      <h1 data-testid='books-container-title' style={{textAlign:'center'}}>Non Friction Books</h1>
+      <button onClick={booksDisplay}>Click Me</button>
+      <div className="books-container" style={{display:'flex',justifyContent:'space-evenly'}}>
         {/* Display all Non-Fiction books here */}
+        {books.map((book,index) => (
+          <BookCard key = {index} props = {book} />
+        ))}
       </div>
     </div>
   );
