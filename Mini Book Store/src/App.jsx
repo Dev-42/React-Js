ef4968cf5ee86f593338211cfbb3d54d57e1c6db
Dev Bhattacharya <71654514+Dev-42@users.noreply.github.com>
2023-08-01 07:15:54 +0530

    Update App.jsx

--- Mini Book Store/src/App.jsx
+++ Mini Book Store/src/App.jsx
@@ -1,12 +1,25 @@
+import NonFiction from "./components/NonFiction";
+import Fiction from './components/Fiction'
+import styles from './App.module.css'
+import {useState} from 'react'
 function App() {
+  let [heading,setHeading] = useState('Show NonFictional Books')
+
+  let toggleHeading = () => {
+    if(heading === 'Show NonFictional Books'){
+      setHeading('Show Fictional Books')
+    }
+    else{
+      setHeading('Show NonFictional Books')
+    }
+  }
   return (
     <div>
-      <h1>Mini Book Store</h1>
-
-      <button data-testid="toggle-btn">{}</button>
-
+      <h1 className={styles.mainHheader}>Mini Book Store</h1>
+      <button data-testid="toggle-btn" onClick={toggleHeading} className={styles.btn}>{heading}</button>
       <div data-testid="conditional-container">
         {/* Render either Fiction or NonFiction Based on the Condition */}
+        {heading === 'Show NonFictional Books' ? <NonFiction props ={heading}/> : <Fiction props ={heading} />}
       </div>
     </div>
   );
